fd7fcbe36792544f068ef729399456a989f4bc0a
Leocaliban <leocaliban@gmail.com>
2018-03-06 21:23:34 -0300

    Interpolação de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Leonardo';
+  idade = 26;
+
+  calcularNascimento(){
+    return 2018 - this.idade;
+  }
 }
 
-/**
- * Instalando Bootstrap
- * npm install bootstrap@next --save
- */

6ad241d334d702d6d5949827c8b53deb73896f3f
Leocaliban <leocaliban@gmail.com>
2018-03-08 12:07:08 -0300

    Classes CSS dinâmicas com ngClass

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -9,6 +9,10 @@ export class FuncionarioCardComponent {
 
   @Input() funcionario: any;
 
+  isAdmin(){
+    return this.funcionario.nome.startsWith('L');
+  }
+  
   getEstilosCartao(){
     return{
       backgroundColor: this.funcionario.id % 2 === 0 ? 'lightblue' : 'lightgreen' 
